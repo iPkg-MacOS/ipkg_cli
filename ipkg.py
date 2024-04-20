b15e14b219719e1f4e4533e1d209af20f07d8238
ERC <149953300+ERC-Innovations@users.noreply.github.com>
2024-04-20 17:40:58 -0400

    Update ipkg.py

--- ipkg.py
+++ ipkg.py
@@ -11,19 +11,38 @@ if sys.platform != 'darwin':
 print("iPkg: Package Manager for Jailbroken MacOS")
 print("Enter Command:")
 print("i: Install Packages")
+print("t: Install Theme")
 print("u: Uninstall Packages")
 print("q: Quit")
 while True:
     cmd = input('ipkg>')
     if cmd == "i":
         pkg = input("Package to install: ")
-        giturl = f"https://github.com/ipkg-macos/iotr/releases/download/v1.0.0/{pkg}.gz"
+        giturl = f"https://github.com/ipkg-macos/iotr/releases/download/v1.0.0/{pkg}.zip"
         data = requests.get(giturl).content
+        if data == b"Not Found":
+            print("Invalid Package")
+            break
         zipfile.ZipFile(io.BytesIO(data)).extractall(f"/opt/packages/{pkg}")
-        shutil.move(pkg, "/opt/packages")
     elif cmd == "u":
         pkg = input("Package to uninstall: ")
         shutil.rmtree(f"/opt/packages/{pkg}")
+    elif cmd == "t":
+        pkg = input("Theme to install: ")
+        giturl = f"https://github.com/ipkg-macos/iotr/releases/download/v1.0.0/{pkg}.zip"
+        data = requests.get(giturl).content
+        if data == b"Not Found":
+            print("Invalid Package")
+            break
+        zipfile.ZipFile(io.BytesIO(data)).extractall(f"/opt/packages/themes/{pkg}")
+        for file in os.listdir(f"/opt/packages/themes/{pkg}"):
+            if file == "Utilities":
+                pass
+            else:
+                os.system(f"cp /opt/packages/themes/{pkg}/{file}/Contents/Resources/*.icns /Applications/{file}/Contents/Resources/")
+        
+        for file in os.listdir(f"/opt/packages/themes/{pkg}/Utilities"):
+            os.system(f"cp /opt/packages/themes/{pkg}/Utilities/{file}/Contents/Resources/*.icns /Applications/Utilities/{file}/Contents/Resources/")
     elif cmd == "q":
         print("Quitting...")
         sys.exit(0)
