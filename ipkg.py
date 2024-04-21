cd3def9af3cb688183c874a90f91c5e3b7f14894
ERC <149953300+ERC-Innovations@users.noreply.github.com>
2024-04-20 20:43:19 -0400

    Update ipkg.py

--- ipkg.py
+++ ipkg.py
@@ -11,9 +11,28 @@ if sys.platform != 'darwin':
 print("iPkg: Package Manager for Jailbroken MacOS")
 print("Enter Command:")
 print("i: Install Packages")
-print("t: Install Theme")
+#print("t: Install Theme")
 print("u: Uninstall Packages")
 print("q: Quit")
+"""elif cmd == "t":
+        pkg = input("Theme to install: ")
+        shutil.copytree(pkg, f"/opt/packages/themes/{pkg.split('/')[-1]}")
+        for file in os.listdir(f"/opt/packages/themes/{pkg.split('/')[-1]}"):
+            print(file)
+            if file == "Utilities":
+                pass
+            elif file[0] == ".":
+                pass
+            else:
+                os.system(f"cp -R /opt/packages/themes/{pkg.split('/')[-1]}/{file}/Contents/Resources/*.icns /Applications/{file}/Contents/Resources")
+        
+        for file in os.listdir(f"/opt/packages/themes/{pkg.split('/')[-1]}/Utilities"):
+            print(file)
+            if file[0] == ".":
+                pass
+            else:
+                os.system(f"cp -R /opt/packages/themes/{pkg.split('/')[-1]}/Utilities/{file}/Contents/Resources/*.icns /Applications/Utilities/{file}/Contents/Resources")"""
+
 while True:
     cmd = input('ipkg>')
     if cmd == "i":
@@ -27,24 +46,6 @@ while True:
     elif cmd == "u":
         pkg = input("Package to uninstall: ")
         shutil.rmtree(f"/opt/packages/{pkg}")
-    elif cmd == "t":
-        pkg = input("Theme to install: ")
-        shutil.copytree(pkg, f"/opt/packages/themes/{pkg.split('/')[-1]}")
-        for file in os.listdir(f"/opt/packages/themes/{pkg.split('/')[-1]}"):
-            print(file)
-            if file == "Utilities":
-                pass
-            elif file[0] == ".":
-                pass
-            else:
-                os.system(f"cp -R /opt/packages/themes/{pkg.split('/')[-1]}/{file}/Contents/Resources/*.icns /Applications/{file}/Contents/Resources/")
-        
-        for file in os.listdir(f"/opt/packages/themes/{pkg.split('/')[-1]}/Utilities"):
-            print(file)
-            if file[0] == ".":
-                pass
-            else:
-                os.system(f"cp -R /opt/packages/themes/{pkg.split('/')[-1]}/Utilities/{file}/Contents/Resources/*.icns /Applications/Utilities/{file}/Contents/Resources/")
     elif cmd == "q":
         print("Quitting...")
         sys.exit(0)
